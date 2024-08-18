f2096019e966398728da32aca72e1db6bebcabb9
alexjamesmx <sg.alex32@gmail.com>
2024-08-18 05:14:03 -0600

    unsafe http

--- utils/browser.js
+++ utils/browser.js
@@ -3,6 +3,7 @@ const { chromium } = require("playwright");
 async function initializeBrowser() {
   const browser = await chromium.launch({
     executablePath: "/app/browsers/chromium-1129/chrome-linux/chrome",
+    ignoreHTTPSErrors: true,
     args: ["--ignore-certificate-errors"],
   });
 
