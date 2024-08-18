a10c9af7a4c8382f57a5aaf2f9a4e57183c69b80
alexjamesmx <sg.alex32@gmail.com>
2024-08-18 01:29:11 -0600

    heroku10

--- utils/browser.js
+++ utils/browser.js
@@ -1,9 +1,7 @@
 const { chromium } = require("playwright");
 
 async function initializeBrowser() {
-  const browser = await chromium.launch({
-    executablePath: "/app/browsers/chromium-1129/chrome-linux/chrome",
-  });
+  const browser = await chromium.launch();
 
   const context = await browser.newContext({
     ignoreHTTPSErrors: true,
